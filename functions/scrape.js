3ceb046e4fc3232b8b0acc7d1227e1fbab98c69d
dragos-vlasie <37800900+dragos-vlasie@users.noreply.github.com>
2023-12-02 11:16:46 +0000

    Update scrape.js

--- functions/scrape.js
+++ functions/scrape.js
@@ -9,7 +9,7 @@ exports.handler = async function (event, context) {
 
     const browser = await puppeteer.launch({
       args: chromium.args,
-      executablePath: process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath,
+      executablePath: await chromium.executablePath,
       headless: true,
     });
 
